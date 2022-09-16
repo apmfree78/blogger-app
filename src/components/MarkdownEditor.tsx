e7de4b6f8803ea3dcc1f283f843da32c90149bc6
apmfree78 <amit@profitswami.com>
2022-09-16 17:25:41 -0400

    added dark mode styling to editor

--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import MDEditor from '@uiw/react-md-editor';
 import rehypeSanitize from 'rehype-sanitize';
+import '../styles/markdownEditor.css';
 
 const MarkdownEditor: React.FC = () => {
   const [content, setContent] = useState('');
 
   return (
-    <div>
+    <div className='text-editor'>
       <MDEditor
         value={content}
         onChange={(c) => setContent(c || '')}
         previewOptions={{
           rehypePlugins: [[rehypeSanitize]],
         }}
+        height={500}
       />
     </div>
   );
