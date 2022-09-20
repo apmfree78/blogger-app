02cd59f859ff60f6285b16d55f851af68f136d45
apmfree78 <amit@profitswami.com>
2022-09-20 18:48:27 -0400

    created bulma modal template that we will use with input forms

---
+++ src/styles/lib/formModal.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface FormModalProps {
+  open: boolean;
+  closeModal: () => void;
+  children: JSX.Element;
+  title: string;
+}
+
+const FormModal: React.FC<FormModalProps> = ({
+  open,
+  closeModal,
+  children,
+  title,
+}) => {
+  return (
+    <div className={`modal ${open ? 'is-active' : ''}`}>
+      <div className='modal-background' onClick={closeModal} />
+      <div className='modal-card'>
+        <header className='modal-card-head'>
+          <p className='modal-card-title'>{title}</p>
+          <button className='delete' onClick={closeModal} />
+        </header>
+        <section className='modal-card-body'>
+          <div className='content'>{children}</div>
+        </section>
+        <footer className='modal-card-foot'>
+          <a className='button' onClick={closeModal}>
+            Cancel
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default FormModal;
