cfec3cec936310257cb186bd64b797a5ee2df567
apmfree78 <amit@profitswami.com>
2022-09-21 20:13:54 -0400

    added useForm hook

---
+++ src/components/forms/useForm.ts
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+
+export default function useForm(initial = {}) {
+  const [inputs, setInputs] = useState(initial);
+  const initialValues = Object.values(initial).join('');
+
+  useEffect(() => {
+    // this functions runs when things we are watching change
+    setInputs(initial);
+  }, [initialValues]);
+
+  function handleChange(e: any) {
+    let { value, name, type } = e.target;
+
+    if (type === 'number') {
+      value = parseInt(value);
+    }
+
+    setInputs({
+      ...inputs, // copying existing state
+      [name]: value, // updating name field with inputed 'value'
+    });
+  }
+
+  function resetForm() {
+    setInputs(initial);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ''])
+    );
+    setInputs(blankState);
+  }
+
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm,
+  };
+}
