4d0541660563d992018a87f83298e19baa0b89e0
apmfree78 <amit@profitswami.com>
2022-11-05 10:39:17 -0400

    add devtoReducer to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import postReducer from "redux/postSlice";
 import mediumReducer from "redux/mediumSlice";
 import hashnodeReducer from "redux/hashnodeSlice";
+import devtoReducer from "redux/devToSlice";
 
 export default configureStore({
   reducer: {
-    posts: postReducer,
+    post: postReducer,
     medium: mediumReducer,
     hashnode: hashnodeReducer,
+    devto: devtoReducer,
   },
 });
