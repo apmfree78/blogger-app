025c53f22f26b57fb5468ca9e0688860688125ea
apmfree78 <amit@profitswami.com>
2022-11-05 15:55:25 -0400

    fix bug with missing redux wrapper on custom jest render function

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -1,11 +1,15 @@
 import React, { FC, ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
 import { GlobalProvider } from "./state/context";
-// import store from './state/store';
-// import { Provider } from 'react-redux';
+import { store } from "redux/store";
+import { Provider } from "react-redux";
 
 const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <GlobalProvider>{children}</GlobalProvider>;
+  return (
+    <Provider store={store}>
+      <GlobalProvider>{children}</GlobalProvider>;
+    </Provider>
+  );
 };
 
 const customRender = (
