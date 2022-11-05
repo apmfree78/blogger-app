8246db7c71e53f04a91241057eeea839f4c47a5f
apmfree78 <amit@profitswami.com>
2022-11-05 16:04:17 -0400

    fix jest mock error in App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "App";
 
+jest.mock("react-redux", () => ({
+  useDispatch: () => {
+    return null;
+  },
+}));
+
 jest.mock("@uiw/react-md-editor", () => () => <div></div>);
 jest.mock("rehype-sanitize", () => () => <div></div>);
-test("renders learn react link", () => {
+test("renders headline", () => {
   render(<App />);
   const linkElement = screen.getByRole("heading");
   expect(linkElement).toBeInTheDocument();
