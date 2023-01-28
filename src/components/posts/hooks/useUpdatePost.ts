afe36b2972a8c8f1b8c25ae1dbb3d22073d088ea
apmfree78 <amit@profitswami.com>
2023-01-28 18:55:43 -0500

    complete useUpdatePost hook and corresponding axios call to update post

--- src/components/posts/hooks/useUpdatePost.ts
+++ src/components/posts/hooks/useUpdatePost.ts
@@ -5,26 +5,23 @@ import { queryKeys } from "react-query/constants";
 import { UseMutateFunction, useMutation, useQueryClient } from "react-query";
 import { customToast } from "components/hooks/useToast";
 import { AxiosResponse } from "axios";
-import { useEffect, useState } from "react";
 
 //fetch user posts with authorization token
-async function updateUserPost(
-  user: User | null,
-  id: string
-): Promise<PostData | null> {
-  if (!user) return null;
-  const { data }: AxiosResponse<PostData> = await axiosInstance.patch(
-    `/collections/posts/records/${id}`,
-    { headers: getJWTHeader(user), data: "ADD DATA HERE" }
+async function updateUserPost(post: Post, user: User | null): Promise<void> {
+  if (!user) return;
+  const { content, publishStatus, author } = post;
+  await axiosInstance.patch(
+    `/collections/posts/records/${post.id}`,
+    { content, publishStatus, author },
+    { headers: getJWTHeader(user) }
   );
-  return data;
 }
 
 export function useUpdatePost() {
   const queryClient = useQueryClient();
   const { user } = useUser();
 
-  const { mutate } = useMutation((id: string) => updateUserPost(user, id), {
+  const { mutate } = useMutation((post: Post) => updateUserPost(post, user), {
     onSuccess: () => {
       queryClient.invalidateQueries([queryKeys.posts]);
       customToast("Post has been updated", "is-success");
