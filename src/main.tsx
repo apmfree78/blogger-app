7932fc8fa0a619fa39694d7aa08edaa4536b3061
apmfree78 <amit@profitswami.com>
2023-02-09 18:25:43 -0500

    switch to vite from CRA

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { queryClient } from "react-query/queryClient";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
 import { store } from "redux/store";
 
 const root = ReactDOM.createRoot(
@@ -27,8 +26,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
