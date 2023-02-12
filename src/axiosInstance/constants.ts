4b70816e99d3c82e3d044dda9bbac6c3d48524a4
apmfree78 <amit@profitswami.com>
2023-02-11 22:26:51 -0500

    restructuure to monorepo

--- src/axiosInstance/constants.ts
+++
@@ -1,1 +0,0 @@
-export const baseUrl = "http://127.0.0.1:8090/api/";
--- src/layout/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactElement } from "react";
-import "styles/App.css";
-
-const Layout: React.FC<{ children: ReactElement }> = ({ children }) => {
-  return <div className="App center">{children}</div>;
-};
-
-export default Layout;
